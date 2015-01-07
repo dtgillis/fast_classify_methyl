76bf4172e4b6b3039384d362acd1a431f71137f5
Daniel Gillis <dtgillis@live.unc.edu>
2015-01-07 14:44:37 -0500

    Changed tuning parameter to 5 crosses and also to minimize 1 - correlation

--- impute_classes_e1071_svm.py
+++ impute_classes_e1071_svm.py
@@ -9,6 +9,8 @@ rpy2.robjects.numpy2ri.activate()
 from scipy.stats import pearsonr
 import argparse
 import time
+from rpy2.robjects import FactorVector
+from rpy2.robjects import r
 
 
 def clean_pearsonr(y_pred, y_test):
@@ -75,32 +77,37 @@ def main_program():
     x_final = x
     y = np_y[:, 1]
     e1071 = importr('e1071')
+    # make a tunning function using correlation
+    error_func = r(
+        'error.func <- function(x,y){ if (is.na(cor(as.numeric(x),as.numeric(y)))) return( 1.0 )  else return(1 - (cor(as.numeric(x),as.numeric(y)))^2) }')
+    tune_func = e1071.tune_control(cross=5, error_fun=error_func)
+    # start up R stuff
     print "starting r"
     print "fitting using cross validation"
-    gamma = 2**np.arange(-1, 1, 1.0)
-    cost = 2**np.arange(-1, 1, 1.0)
+    gamma = 2**np.arange(-1, 0, 1.0)
+    cost = 2**np.arange(-1, 0, 1.0)
     # fit model using tune in R package.
-    fit = e1071.tune_svm(x=x_final, y=y, gamma=gamma, cost=cost)
-    best_model = fit.rx2('best.model')
+    fit = e1071.tune_svm(x=x_final, y=FactorVector(y), gamma=gamma, cost=cost, tunecontrol=tune_func)
+    #best_model = fit.rx2('best.model')
     total_time = time.time() - start_time
-    print "Finished fitting now cross validate for score"
+    #print "Finished fitting now cross validate for score"
     # now make a couple predictions using same data to get r squared
-    skf = cross_validation.StratifiedKFold(y, n_folds=5)
-    scores = []
-    for train_index, test_index in skf:
-
-        y_test = y[test_index]
-        x_test = x_final[test_index]
-
-        y_pred = np.array(e1071.predict_svm(best_model, x_test))
-
-        scores.append(clean_pearsonr(y_pred, y_test))
-
+    # scores = []
+    # for train_index, test_index in skf:
+    #
+    #     y_test = y[test_index]
+    #     x_test = x_final[test_index]
+    #
+    #     y_pred = np.array(e1071.predict_svm(best_model, x_test))
+    #
+    #     scores.append(clean_pearsonr(y_pred, y_test))
+    # what were the results ?
     best_parameters = fit.rx('best.parameters')[0]
-
+    best_performance = 1.0 - float(fit.rx('best.performance')[0][0])
     gamma = float(best_parameters.rx('gamma')[0][0])
     cost = float(best_parameters.rx('cost')[0][0])
-    out = '{0:s} {1:f} {2:f} gamma {3:f} cost {4:f}'.format(args.snp_name[0], np.mean(scores), total_time, gamma, cost)
+    # write out results.
+    out = '{0:s} {1:f} {2:f} gamma {3:f} cost {4:f}'.format(args.snp_name[0], best_performance, total_time, gamma, cost)
     out += os.linesep
     f = open(args.out_file[0], mode='ab')
     f.write(out)
